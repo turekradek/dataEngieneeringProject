e0cc3ad8a70604cbd8ccd10de277e93c186b4379
turekradek <turekradek@gmail.com>
2025-01-18 14:38:17 +0100

    a

--- project/flask_app/app/app.py
+++ project/flask_app/app/app.py
@@ -4,6 +4,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 import os 
 import logging
 from datetime import datetime
+import pandas as pd 
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -55,69 +56,109 @@ def filesjson():
     #     for line in lines:
     #         print(line)
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
     return f"Reading Json files : <br> {current_time} <br>"
 
 @app.route('/bronze')
-def files():
+def filesbronze():
     filespath = '../bronze'
     fileslist = os.listdir(filespath)
-    print( fileslist )
     logger.info(f"This is an info message: Filelist bronze: {fileslist}")
-    # with open(f'{filescsvpath}/file1.csv') as f:
-    #     lines = f.readlines()
-    #     for line in lines:
-    #         print(line)
+    
+    data = []
+    for idx, file in enumerate(fileslist):
+        name, extension = os.path.splitext(file)
+        timestamp = datetime.fromtimestamp(os.path.getmtime(os.path.join(filespath, file))).strftime("%Y-%m-%d %H:%M:%S")
+        data.append([idx, name, extension, timestamp])
+    
+    df = pd.DataFrame(data, columns=['id', 'namefile', 'extension', 'timestamp'])
+    df = df.sort_values(by=['extension', 'namefile'])
+    
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"Reading bronze files <br> {current_time} <br> {df.to_html()}"
 
-    return f"Reading bronze files <br> {current_time} "
+@app.route('/silver')
+def filessilver():
+    filespath = '../silver'
+    fileslist = os.listdir(filespath)
+    logger.info(f"This is an info message: Filelist silver: {fileslist}")
+    
+    data = []
+    for idx, file in enumerate(fileslist):
+        name, extension = os.path.splitext(file)
+        timestamp = datetime.fromtimestamp(os.path.getmtime(os.path.join(filespath, file))).strftime("%Y-%m-%d %H:%M:%S")
+        data.append([idx, name, extension, timestamp])
+    
+    df = pd.DataFrame(data, columns=['id', 'namefile', 'extension', 'timestamp'])
+    df = df.sort_values(by=['extension', 'namefile'])
+    
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"Reading silver files <br> {current_time} <br> {df.to_html()}"
 
-# @app.route('/silver')
-# def filessilver():
-#     filespath = '../silver'
-#     fileslist = os.listdir(filespath)
-#     print( fileslist )
-#     logger.info(f"This is an info message: Filelist silver: {fileslist}")
-#     # with open(f'{filescsvpath}/file1.csv') as f:
-#     #     lines = f.readlines()
-#     #     for line in lines:
-#     #         print(line)
-#     return "Reading silver files"
+@app.route('/gold')
+def filesgold():
+    filespath = '../gold'
+    fileslist = os.listdir(filespath)
+    logger.info(f"This is an info message: Filelist gold: {fileslist}")
+    
+    data = []
+    for idx, file in enumerate(fileslist):
+        name, extension = os.path.splitext(file)
+        timestamp = datetime.fromtimestamp(os.path.getmtime(os.path.join(filespath, file))).strftime("%Y-%m-%d %H:%M:%S")
+        data.append([idx, name, extension, timestamp])
+    
+    df = pd.DataFrame(data, columns=['id', 'namefile', 'extension', 'timestamp'])
+    df = df.sort_values(by=['extension', 'namefile'])
+    
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"Reading gold files <br> {current_time} <br> {df.to_html()}"
 
-# @app.route('/gold')
-# def filesgold():
-#     filespath = '../gold'
-#     fileslist = os.listdir(filespath)
-#     print( fileslist )
-#     logger.info(f"This is an info message: Filelist gold: {fileslist}")
-#     # with open(f'{filescsvpath}/file1.csv') as f:
-#     #     lines = f.readlines()
-#     #     for line in lines:
-#     #         print(line)
-#     return "Reading gold files"
+@app.route('/scripts')
+def filesscripts():
+    filespath = '../scripts'
+    fileslist = os.listdir(filespath)
+    logger.info(f"This is an info message: Filelist scripts: {fileslist}")
+    
+    data = []
+    for idx, file in enumerate(fileslist):
+        name, extension = os.path.splitext(file)
+        timestamp = datetime.fromtimestamp(os.path.getmtime(os.path.join(filespath, file))).strftime("%Y-%m-%d %H:%M:%S")
+        data.append([idx, name, extension, timestamp])
+    
+    df = pd.DataFrame(data, columns=['id', 'namefile', 'extension', 'timestamp'])
+    df = df.sort_values(by=['extension', 'namefile'])
+    
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"Reading scripts files <br> {current_time} <br> {df.to_html()}"
 
-# @app.route('/scripts')
-# def filesscripts():
-#     filespath = '../scripts'
-#     fileslist = os.listdir(filespath)
-#     print( fileslist )
-#     logger.info(f"This is an info message: Filelist gold: {fileslist}")
-#     # with open(f'{filescsvpath}/file1.csv') as f:
-#     #     lines = f.readlines()
-#     #     for line in lines:
-#     #         print(line)
-#     return "Reading scripts files"
+@app.route('/dockervolume')
+def filessdockervolume():
+    filespath = '../docker_volume'
+    fileslist = os.listdir(filespath)
+    logger.info(f"This is an info message: Filelist docker_volume: {fileslist}")
+    
+    data = []
+    for idx, file in enumerate(fileslist):
+        name, extension = os.path.splitext(file)
+        timestamp = datetime.fromtimestamp(os.path.getmtime(os.path.join(filespath, file))).strftime("%Y-%m-%d %H:%M:%S")
+        data.append([idx, name, extension, timestamp])
+    
+    df = pd.DataFrame(data, columns=['id', 'namefile', 'extension', 'timestamp'])
+    df = df.sort_values(by=['extension', 'namefile'])
+    
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"Reading docker_volume files <br> {current_time} <br> {df.to_html()}"
 
-# @app.route('/dockervolume')
-# def filesscripts():
-#     filespath = '../docker_volume'
-#     fileslist = os.listdir(filespath)
-#     print( fileslist )
-#     logger.info(f"This is an info message: Filelist gold: {fileslist}")
-#     # with open(f'{filescsvpath}/file1.csv') as f:
-#     #     lines = f.readlines()
-#     #     for line in lines:
-#     #         print(line)
-#     return "Reading docker_volume files"
+@app.route('/routes')
+def show_routes():
+    routes = []
+    for rule in app.url_map.iter_rules():
+        routes.append(f"{rule.endpoint}: {rule.rule}")
+    return "<br>".join(routes)
+
+def files_list( path ):
+    fileslist = os.listdir(path)
+    return fileslist
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
